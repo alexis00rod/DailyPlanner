812de5794bef75f87a817d9af407f7560e7f431d
alexis00rod <alexis00rodrigo@gmail.com>
2022-11-25 12:10:15 -0300

    Configure rutas principales

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { App } from './App'
 import './index.css'
+import { AddTasks } from './pages/AddTasks'
+import { Profile } from './pages/Profile'
+import { Settings } from './pages/Settings'
+import { Tasks } from './pages/Tasks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />}>
+                    <Route index element={<Tasks />}></Route>
+                    <Route path='profile' element={<Profile />}></Route>
+                    <Route path='add-task' element={<AddTasks />}></Route>
+                    <Route path='settings' element={<Settings />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 )
