2940564698fe708a11c89d9c97694488a2444859
alexis00rod <alexis00rodrigo@gmail.com>
2022-11-28 17:04:19 -0300

    Agregue plugin tw-elements

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      './node_modules/tw-elements/dist/js/**/*.js'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/line-clamp'),
+        require('tw-elements/dist/plugin')
+    ],
 }
