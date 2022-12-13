3b51a728f7e7a60365b9225c73907ebdfdd288ab
alexis00rod <alexis00rodrigo@gmail.com>
2022-12-13 00:19:56 -0300

    Cambie estilos

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,8 @@ import { Main } from '../components/Main'
 import { Message } from '../components/Message'
 import { login } from '../service/auth'
 
+import formImage from '../assets/form-image.png'
+
 export const Login = () => {
     const [userToLogin, setUserToLogin] = useState()
     const [errorMessage, setErrorMessage] = useState()
@@ -26,34 +28,46 @@ export const Login = () => {
 
     return (
         <Main full>
-            <div className='section'>
-                <div className='w-full px-2 py-2 flex flex-col items-center'>
-                    <h2 className='w-max mb-2 px-1 py-2 text-4xl font-bold'>Login</h2>
-                    <p className="w-max px-1 py-1 flex items-center gap-2">You are not a user?<Link to='/signup' className="text-slate-700 font-semibold duration-150 hover:text-slate-900">Signup</Link></p>
+            <div className='section section-row'>
+                <div className='hidden lg:flex lg:grow'>
+                    <img src={formImage} alt="" className='w-full h-full object-cover'/>
                 </div>
-                <form className='' onSubmit={loginUser}>
-                    {errorMessage && <Message color="red" message={errorMessage} />}
-                    {/* User email */}
-                    <div className='px-1 py-1 flex flex-col gap-1'>
-                        <label htmlFor="email" className="px-2 py-1 text-sm font-semibold cursor-pointer">Email</label>
-                        <input type="text" name='email' id='email' className='h-10 px-3 bg-slate-100 border-2 border-slate-500 rounded-lg shadow-inner focus:outline-none focus:border-cyan-500 focus:shadow-lg' onChange={handleUserToLogin} required/>
+                {/* Login */}
+                <div className='w-full lg:w-1/3 py-4 flex flex-col gap-2 lg:flex-none'>
+                    {/* Login header */}
+                    <div className='w-full px-1 py-2 flex flex-col'>
+                        <h1 className='mb-1 px-1 pt-2 pb-4 text-5xl font-bold font-pacifico text-teal-500'>Daily Planner</h1>
+                        <h2 className='px-2 pt-2 text-slate-500 text-xl'>Iniciar sesion</h2>
                     </div>
-                    {/* User password */}
-                    <div className='px-1 py-1 flex flex-col gap-1'>
-                        <label htmlFor="password" className="px-2 py-1 text-sm font-semibold cursor-pointer">Password</label>
-                        <input type="password" name='password' id='password' className='h-10 px-3 bg-slate-100 border-2 border-slate-500 rounded-lg shadow-inner focus:outline-none focus:border-cyan-500 focus:shadow-lg' onChange={handleUserToLogin} required/>
+                    {/* Login body */}
+                    <div className='w-full px-1 py-1 flex flex-col gap-2' onSubmit={loginUser}>
+                        {/* Login form */}
+                        <form className='w-full flex flex-col gap-2'>
+                            {/* Login email */}
+                            <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                <label htmlFor="email" className="px-1 font-medium">Email</label>
+                                <input type="text" name='email' id='email' className='input-text' onChange={handleUserToLogin} required/>
+                            </div>
+                            {/* Login password */}
+                            <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                <label htmlFor="password" className="px-1 font-medium">Password</label>
+                                <input type="password" name='password' id='password' className='input-text' onChange={handleUserToLogin} required/>
+                            </div>
+                            {/* Login forgot password */}
+                            <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                <span className='w-max px-1 flex items-center text-teal-500 duration-300 hover:text-teal-600 cursor-pointer'>Forgot password?</span>
+                            </div>
+                            {/* Submit */}
+                            <div className='w-full px-1 py-1'>
+                                <button type='submit' className='w-full h-10 mx-auto px-2 flex items-center justify-center text-slate-100 font-semibold bg-teal-500 rounded-lg duration-300 hover:bg-teal-600'>Login</button>
+                            </div>
+                        </form>
+                        {/* Go to login */}
+                        <div className='px-2 h-16 flex items-center'>
+                            <h4 className='px-1 py-2 w-max flex items-center gap-1'>¿No tienes cuenta?<Link to='/signup' className='w-max px-1 flex items-center text-teal-500 duration-300 hover:text-teal-600'>Crear cuenta</Link></h4>
+                        </div>
                     </div>
-                    <div className='px-1 py-1'>
-                        <span className='px-1 text-slate-600 font-medium cursor-pointer duration-150 hover:text-slate-900'>Forgot password?</span>
-                    </div>
-                    {/* Submit */}
-                    <div className='px-1 py-2'>
-                        <button type='submit' className='w-max h-9 mx-auto px-4 flex items-center rounded-full bg-slate-600 text-slate-100 font-semibold duration-150 hover:bg-slate-900'>Login</button>
-                    </div>
-                </form>
-                {/* <div className='w-full px-1 py-1 flex items-center justify-center'>
-                    <button className='w-10 h-10 flex items-center justify-center bg-red-500 text-slate-100 rounded-full duration-150 hover:bg-red-600'><i className="fa-brands fa-google"></i></button>
-                </div> */}
+                </div>
             </div>
         </Main>
     )
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,87 +1,130 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Main } from '../components/Main'
 import { Message } from '../components/Message'
 import { signup } from '../service/auth'
 import { addUserToDb } from '../service/firestore'
+import formImage from '../assets/form-image.png'
+import { getDefaultProfilePhotoURL, uploadUserPhoto } from '../service/storage'
 
 export const Signup = () => {
-    const [userToSignup, setUserToSignup] = useState({})
-    const [userPhoto, setUserPhoto] = useState("")
-    const [errorMessage, setErrorMessage] = useState()
-    const navigate = useNavigate()
+    const [signupStep, setSignupStep] = useState(1)
+    const [signupUserPhoto, setSignupUserPhoto] = useState(false)
 
-    const signupUser = async e => {
+    const handleSignupUserPhoto = ({target:{files}}) => {
+        setSignupUserPhoto(false)
+        uploadUserPhoto(files[0],setSignupUserPhoto)
+    }
+
+    const handleSignupEmailPassword = e => {
         e.preventDefault()
-        setErrorMessage("")
-        try {
-            await signup(userToSignup)
-            await addUserToDb(userToSignup)
-            navigate('/login')
-        } catch (err) {
-            setErrorMessage(err.code)
-        }
+        setSignupStep(2)
     }
 
-    const handleUserToSignup = ({target:{name,value}}) => {
-        setUserToSignup({
-            ...userToSignup,
-            [name]:value
-        })
+    const handleSignupProfile = e => {
+        e.preventDefault()
+        setSignupStep(1)
     }
 
-    const handleUserPhoto = ({target:{files}}) => {
-        setUserPhoto(files[0])
-    }
+    useEffect(() => {
+        getDefaultProfilePhotoURL(setSignupUserPhoto)
+    },[])
 
     return (
         <Main full>
-            <div className='section'>
-                <div className='w-full px-2 py-2 flex flex-col items-center gap-1'>
-                    <h2 className='w-max mb-1 px-2 py-2 text-4xl font-bold'>Signup</h2>
-                    <p className="w-max px-2 py-1 flex items-center gap-2">Are you a user? <Link to='/' className="text-slate-700 font-semibold duration-150 hover:text-slate-900">Login</Link></p>
+            <div className='section section-row'>
+                <div className='hidden lg:flex lg:grow'>
+                    <img src={formImage} alt="" className='w-full h-full object-cover'/>
                 </div>
-                <form className='w-full mx-auto px-2 py-2 flex flex-col justify-evenly md:justify-start md:gap-2' onSubmit={signupUser}>
-                    {/* Error message */}
-                    {errorMessage && <Message color="red" message={errorMessage} />}
-                    {/* User name */}
-                    {/* <div className='px-1 py-1 flex flex-col gap-1'>
-                        <label htmlFor="name" className="px-2 py-1 text-sm font-semibold cursor-pointer">Name</label>
-                        <input type="text" name='name' id='name' className='h-10 px-3 bg-slate-100 border-2 border-slate-500 rounded-lg shadow-inner focus:outline-none focus:border-cyan-500 focus:shadow-lg' onChange={handleUserToSignup} required/>
-                    </div> */}
-                    {/* User email */}
-                    <div className='px-1 py-1 flex flex-col gap-1'>
-                        <label htmlFor="email" className="px-2 py-1 text-sm font-semibold cursor-pointer">Email</label>
-                        <input type="text" name='email' id='email' className='h-10 px-3 bg-slate-100 border-2 border-slate-500 rounded-lg shadow-inner focus:outline-none focus:border-cyan-500 focus:shadow-lg' onChange={handleUserToSignup} required/>
+                {/* Signup */}
+                <div className='w-full lg:w-1/3 py-4 flex flex-col gap-2 lg:flex-none'>
+                    {/* Signup header */}
+                    <div className='w-full px-1 py-2 flex flex-col'>
+                        <h1 className='mb-1 px-1 pt-2 pb-4 text-5xl font-bold font-pacifico text-teal-500'>Daily Planner</h1>
+                        <h2 className='px-2 pt-2 text-slate-500 text-xl'>Crear cuenta</h2>
                     </div>
-                    {/* User password */}
-                    <div className='px-1 py-1 flex flex-col gap-1'>
-                        <label htmlFor="password" className="px-2 py-1 text-sm font-semibold cursor-pointer">Password</label>
-                        <input type="password" name='password' id='password' className='h-10 px-3 bg-slate-100 border-2 border-slate-500 rounded-lg shadow-inner focus:outline-none focus:border-cyan-500 focus:shadow-lg' onChange={handleUserToSignup} required/>
+                    {/* Signup body */}
+                    <div className='w-full px-1 py-1 flex flex-col items-center gap-2'>
+                        {/* Signup steps */}
+                        <div className='w-full flex px-1 py-1'>
+                            {/* Step 1 */}
+                            <div className='grow px-1 py-1'>
+                                <span className={`mb-1 w-10 h-10 flex items-center justify-center text-xl rounded-lg border-2 border-teal-300 ${signupStep === 1 ? "border-teal-300 text-slate-100 bg-teal-300" : "border-teal-300 text-teal-300"}`}>
+                                    {signupStep === 1 ? "1" : <i className='fa-solid fa-check'></i>}
+                                </span>
+                                <span className='text-sm text-slate-500 text-center'>Email y contraseña</span>
+                            </div>
+                            <div className='grow px-1 py-1'>
+                                <span className='w-10 h-10 flex items-center justify-center'>
+                                    <i className='fa-solid fa-chevron-right text-xl text-slate-500'></i>
+                                </span>
+                            </div>
+                            {/* Step 2 */}
+                            <div className='grow px-1 py-1'>
+                                <span className={`mb-1 w-10 h-10 flex items-center justify-center text-xl rounded-lg ${signupStep === 2 ? "bg-teal-300 text-slate-100" : "bg-slate-200 text-slate-500"}`}>2</span>
+                                <span className='text-sm text-slate-500 text-center'>Perfil</span>
+                            </div>
+                        </div>
+                        {/* Signup form */}
+                        <form className='w-full flex flex-col gap-2'>
+                            {/* Signup form steps navigation */}
+                            <div className='w-full h-56'>
+                                {signupStep === 1
+                                // Step 1
+                                ?   <>
+                                        {/* Signup email */}
+                                        <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                            <label htmlFor="email" className='px-1 font-medium'>Email</label>
+                                            <input type="email" name="email" id='email'  className='input-text' />
+                                        </div>
+                                        {/* Signup password */}
+                                        <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                            <label htmlFor="password1" className='px-1 font-medium'>Contraseña</label>
+                                            <input type="password" name="password1"  className='input-text' />
+                                        </div>
+                                        {/* Signup confirm password */}
+                                        <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                            <label htmlFor="password2" className='px-1 font-medium'>Confirmar contraseña</label>
+                                            <input type="password" name="password2" className='input-text' />
+                                        </div>
+                                    </>
+                                // Step 2
+                                :   <>
+                                        {/* Signup profile photo */}
+                                        <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                            <span className='px-1 font-medium'>Foto de perfil</span>
+                                                {signupUserPhoto 
+                                                ?   <div className='w-full px-1 py-1 flex '>
+                                                        <label htmlFor="photoURL" className='w-20 h-20 bg-teal-500 px-1 py-1 rounded-full overflow-hidden duration-300 cursor-pointer hover:bg-teal-600'>
+                                                            <img src={signupUserPhoto} alt="" className='w-full h-full rounded-full object-cover' />
+                                                        </label>
+                                                        <input type="file" name='photoURL' id='photoURL' className='hidden' onChange={handleSignupUserPhoto} />
+                                                    </div>
+                                                :   <div className='w-full px-1 py-1 flex'>
+                                                        <div className="w-20 h-20 inline-block text-teal-500 border-4 rounded-full spinner-border animate-spin "></div>
+                                                    </div>}
+                                        </div>
+                                        {/* Signup name */}
+                                        <div className='w-full mb-1 px-1 py-1 flex flex-col'>
+                                            <label htmlFor="name" className='px-1 font-medium'>Nombre</label>
+                                            <input type="text" name="name" id='name' placeholder='Nombre' className='input-text' />
+                                        </div>
+                                    </>}
+                            </div>
+                            {/* Signup button step */}
+                            <div className='w-full px-1 py-1'>
+                                {signupStep === 1
+                                ?   <button className='w-full h-10 mx-auto px-2 flex items-center justify-center text-slate-100 font-semibold bg-teal-500 rounded-lg duration-300 hover:bg-teal-600' onClick={handleSignupEmailPassword}>Continuar</button>
+                                :   <button className='w-full h-10 mx-auto px-2 flex items-center justify-center text-slate-100 font-semibold bg-teal-500 rounded-lg duration-300 hover:bg-teal-600' onClick={handleSignupProfile}>Finalizar</button>}
+                            </div>
+                        </form>
                     </div>
-                    {/* User photo */}
-                    {/* <div className='px-1 py-1 flex flex-col gap-1'>
-                        <label htmlFor="photo" className='px-2 py-1 text-sm font-semibold cursor-pointer'>Photo</label>
-                        <input type="file" name="photo" id="photo" className='input-file' onChange={handleUserPhoto} />
-                        <img src="" alt="" />
-                    </div> */}
-                    {/* User birth */}
-                    {/* <div className='px-1 py-1 flex flex-col'>
-                        <label htmlFor="taskDay" className="px-2 py-1 text-sm font-semibold cursor-pointer">Birth</label>
-                        <input 
-                        type="date" 
-                        name="birth" 
-                        id='birth' 
-                        className='h-10 px-3 bg-slate-100 border-2 border-slate-500 rounded-lg shadow-inner focus:outline-none focus:border-cyan-500 focus:shadow-lg'
-                        onChange={handleUserToSignup}
-                        required
-                        />
-                    </div> */}
-                    {/* Submit */}
-                    <div className='px-1 py-2'>
-                        <button type='submit' className='w-max h-9 mx-auto px-4 flex items-center rounded-full bg-slate-600 text-slate-100 font-semibold duration-150 hover:bg-slate-900'>Sign up</button>
+                    {/* Back login */}
+                    <div className='px-2 h-16 flex items-center'>
+                        <h4 className='px-1 py-2 w-max flex items-center gap-1'>¿Ya tienes una cuenta?<Link to='/login' className='w-max px-1 flex items-center text-teal-500 duration-300 hover:text-teal-600'>Iniciar sesion</Link></h4>
                     </div>
-                </form>
+                </div>
             </div>
         </Main>
     )
