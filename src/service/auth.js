5efd2b60c581cc82e78e6c9d7bc114a7ec744665
alexis00rod <alexis00rodrigo@gmail.com>
2022-12-13 15:20:01 -0300

    Modifique funcion signup agregando actualizando de perfil

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,11 +1,17 @@
 import { app } from './key'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, setPersistence,browserLocalPersistence } from 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, setPersistence,browserLocalPersistence, updateProfile } from 'firebase/auth'
 
 export const auth = getAuth(app)
 
 // Funcion para crear usuario usando email y contraseña
-export const signup = ({email,password}) => {
-    return createUserWithEmailAndPassword(auth,email,password)
+export const signup = (email,password,displayName,photoURL) => {
+    createUserWithEmailAndPassword(auth,email,password)
+    .then(credential => {
+        updateProfile(auth.currentUser, {
+            displayName,
+            photoURL
+        })
+    })
 }
 
 // Funcion para iniciar sesion usando email y contraseña
