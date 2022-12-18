7ee4936c72490f445bee361098c980935d71befa
alexis00rod <alexis00rodrigo@gmail.com>
2022-12-18 04:53:25 -0300

    Limpie codigo

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -74,7 +74,7 @@ export const Calendar = () => {
                     </div>
                 </div>
                 {/* Tareas del dia */}
-                <ul className='grow grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-rows-3 gap-4'>
+                <ul className='px-1 py-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                     {tasksList(dayTasks,statusTasks)}
                 </ul>
             </Main>
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react"
 import { Header, Main } from '../components/index'
-import { useTheme } from "../hook/useTheme"
+import { useThemeContext } from "../context/ThemeContext"
 import { logout } from '../service/auth'
 
 export const Settings = () => {
-    const { theme,setTheme } = useTheme()
+    const { theme, handleTheme } = useThemeContext()
 
     return (
         <>
@@ -15,12 +15,12 @@ export const Settings = () => {
                     <div className="w-full max-w-screen-sm mx-auto px-2 py-2 flex flex-wrap gap-2">
                         <h4 className="px-2 py-1 font-semibold">Tema: </h4>
                         <div className="px-2 flex justify-center gap-4 grow">
-                            <div className={`w-full max-w-xs px-3 py-3 bg-slate-200 rounded-lg cursor-pointer ${theme && "border-4 border-green-500"}`} onClick={() => setTheme(true)}>
+                            <div className={`w-full max-w-xs px-3 py-3 bg-slate-200 rounded-lg cursor-pointer ${theme === "light" && "border-4 border-green-500"}`} onClick={handleTheme}>
                                 <div className="px-3 py-3 bg-slate-100 rounded-lg">
                                     <span className="text-slate-900">Claro</span>
                                 </div>
                             </div>
-                            <div className={`w-full max-w-xs px-3 py-3 bg-slate-800 rounded-lg cursor-pointer ${!theme && "border-4 border-green-500"}`} onClick={() => setTheme(false)}>
+                            <div className={`w-full max-w-xs px-3 py-3 bg-slate-800 rounded-lg cursor-pointer ${theme === "dark" && "border-4 border-green-500"}`} onClick={handleTheme}>
                                 <div className="px-3 py-3 bg-slate-900 rounded-lg">
                                     <span className="text-slate-100">Oscuro</span>
                                 </div>
