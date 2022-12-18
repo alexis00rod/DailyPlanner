58d0747cc13e330f187882f4cb93a25e6cee9842
alexis00rod <alexis00rodrigo@gmail.com>
2022-12-18 00:42:02 -0300

    Agregue filtro de tareas por dia

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,8 @@
-import { useState,useEffect } from 'react'
 import { NavLink, useParams } from 'react-router-dom'
 import { useUserContext } from '../context/UserContext'
-import { Header, Main, TaskItem } from '../components/index'
+import { Header, Main, TaskList } from '../components/index'
 import { useTasks } from '../hook/useTasks'
-
-const tasksList = (tasks,completed) => {
-    const tasksFilter = tasks.filter(e => e.completed === completed)
-    return tasksFilter.map(item => <TaskItem key={item.id} item={item} />)
-}
+import { useState } from 'react'
 
 const NavbarLink = ({children,...props}) => {
     return <NavLink {...props} 
@@ -17,16 +12,20 @@ const NavbarLink = ({children,...props}) => {
 }
 
 export const Home = () => {
-    const {userLogged} = useUserContext()
-    const {category} = useParams()
-    const {allTasks,workTasks,personalTasks,otherTasks} = useTasks()
-    const [statusTasks, setStatusTasks] = useState(false)
+    const { userLogged } = useUserContext()
+    const { category } = useParams()
+    const { allTasks, workTasks, personalTasks, otherTasks} = useTasks()
+    const [filterTasks, setFilterTasks] = useState("today")
+
+    const handleFilterTasks = ({target:{value}}) => {
+        setFilterTasks(value)
+    }
 
     return <>
             <Header title={`Hola! ${userLogged.displayName}`} />
             <Main>
                 {/* Filtrar por categorias */}
-                <div className='px-1 py-1 grid grid-cols-2 gap-2 sm:grid-cols-4'>
+                <div className='px-1 py-1 grid grid-cols-2 gap-4 sm:grid-cols-4'>
                     {[["/","Tareas",allTasks],["/work","Trabajo",workTasks],["/personal","Personal",personalTasks],["/other","Otros",otherTasks]].map((item,i) => (
                         <NavbarLink to={item[0]} key={i}>
                             {item[1]} <span className='text-3xl'>{item[2].length}</span>
@@ -36,31 +35,20 @@ export const Home = () => {
                 {/* Filtrar por estado */}
                 <div className='w-full px-1 py-1 flex items-center gap-2'>
                     <div className='input-radio'>
-                        <input type="radio" name="status" id="pending" className="input-radio" onChange={() => setStatusTasks(false)} defaultChecked/>
+                        <input type="radio" name="filter" id="today" value="today" onChange={handleFilterTasks}  defaultChecked/>
+                        <label htmlFor="today">Hoy</label>
+                    </div>
+                    <div className='input-radio'>
+                        <input type="radio" name="filter" id="pending" value="pending" onChange={handleFilterTasks} />
                         <label htmlFor="pending">Pendiente</label>
                     </div>
                     <div className='input-radio'>
-                        <input type="radio" name="status" id="finish" className="input-radio" onChange={() => setStatusTasks(true)}/>
+                        <input type="radio" name="filter" id="finish" value="finish" onChange={handleFilterTasks} />
                         <label htmlFor="finish">Finalizado</label>
                     </div>
                 </div>
                 {/* Lista de tareas */}
-                <ul className='px-1 py-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-2'>
-                    {category === undefined
-                    ?   tasksList(allTasks,statusTasks)
-                    :   category === "work"
-                        ?   tasksList(workTasks,statusTasks)
-                        :   category === "personal"
-                            ?   tasksList(personalTasks,statusTasks)
-                            :   tasksList(otherTasks,statusTasks)
-                    }
-                    {/*                     
-                        !category && tasksList(allTasks,statusTasks)
-                        category === "work" && tasksList(workTasks,statusTasks)
-                        category === "personal" && tasksList(personalTasks,statusTasks)
-                        category === "other" && tasksList(otherTasks,statusTasks)
-                     */}
-                </ul>
+                <TaskList category={category} filter={filterTasks} />
             </Main>
         </>
 }
